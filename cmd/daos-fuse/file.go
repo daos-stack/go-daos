d898a7693cc9c475125df5b184deb64f8ff83c18
Michael MacDonald <michael.macdonald@intel.com>
2017-03-07 07:57:58 -0800

    Add missing file
    Forgot to include this in previous push.

---
+++ cmd/daos-fuse/file.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+
+	"bazil.org/fuse"
+	"bazil.org/fuse/fs"
+	"github.com/daos-stack/go-daos/pkg/daosfs"
+	"github.com/intel-hpdd/logging/debug"
+)
+
+// FileHandle represents an open file handle
+type FileHandle struct {
+	handle *daosfs.FileHandle
+}
+
+// File represents a file node
+type File struct {
+	Node
+}
+
+// NewFile returns a *File
+func NewFile(node *daosfs.DaosNode) *File {
+	return &File{
+		Node: Node{
+			node: node,
+		},
+	}
+}
+
+func (f *File) Open(ctx context.Context, req *fuse.OpenRequest, res *fuse.OpenResponse) (fs.Handle, error) {
+	h, err := f.node.Open(uint32(req.Flags))
+	return &FileHandle{handle: h}, err
+}
+
+func (fh *FileHandle) Write(ctx context.Context, req *fuse.WriteRequest, res *fuse.WriteResponse) error {
+	n, err := fh.handle.Write(req.Offset, req.Data)
+	res.Size = int(n)
+
+	if err != nil {
+		debug.Printf("error in Write(): %s", err)
+	}
+	return err
+}
+
+func (fh *FileHandle) Read(ctx context.Context, req *fuse.ReadRequest, res *fuse.ReadResponse) error {
+	return fh.handle.Read(req.Offset, int64(req.Size), &res.Data)
+}
